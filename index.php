6866e8543977d9142bf2c58d603787ebd17a4802
rfelipe <f.questao@gmail.com>
2025-04-14 20:04:26 -0300

    inicia busca cep

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+$arquivo = 'entrada.txt';
+$cep = null;
+
+function organizaArquivoCidades($arquivo) {
+    $linhas = file($arquivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $faixas = [];
+
+
+    foreach ($linhas as $linha) {
+        if (trim($linha) === '--') {
+            $proximaLinhaEhOCep = true;
+            continue;
+        }
+
+        if (isset($proximaLinhaEhOCep) && $proximaLinhaEhOCep) {
+            $cep = (int)trim($linha);
+            break;
+        }
+
+        list($cidade, $inicio, $fim) = explode(',', $linha);
+        $faixas[] = [
+            'cidade' => $cidade,
+            'inicio' => (int)$inicio,
+            'fim' => (int)$fim
+        ];
+    }
+
+    return $faixas;
+}
+
+function procurarCidade() {
+
+    $faixas =  organizaArquivoCidades($arquivo);
+
+    foreach ($faixas as $faixa) {
+        if ($cep >= $faixa['inicio'] && $cep <= $faixa['fim']) {
+            return $faixa['cidade'];
+        }
+    }
+    return "não foi encontrada nenhuma faixa.";
+}
+
+echo procurarCidade();
